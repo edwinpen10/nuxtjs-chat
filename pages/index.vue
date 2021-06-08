e89e61dd3702c92e5824e19308ce1e85f18321c5
edwin <edwinpen010@gmail.com>
2021-06-08 17:00:50 +0700

    on prigress

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,28 @@
       <div class="col-md-4">
         <div v-for="list in chatlist" :key="list.id">
           <div v-for="data in list.data.data" :key="data.id">
-                 <button class="btn btn-sm btn-primary mb-2" @click="getMessage(data.id)">{{data.user[0].name}}</button>
+                 <button class="btn btn-sm btn-primary mb-2" @click="getMessage(data.room_id)">{{data}}</button>
           </div>
         </div>
       </div>
       <div class="col-md-8">
         <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
       <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-        <input class="fs-5 fw-semibold" v-model="username"/>
+        <h4>Chat yuk borr</h4>
       </div>
       <div class="list-group list-group-flush border-bottom scrollarea">
         <div class="list-group-item list-group-item-action py-3 lh-tight"
              v-for="message in messages" :key="message.id"
         >
-          <div class="d-flex w-100 align-items-center justify-content-between">
-            <strong class="mb-1">{{ message.user[0].name }}</strong>
+        <div class="card">
+          <div class="card-body">
+              <div class="d-flex w-100 align-items-center justify-content-between">
+            <strong class="mb-1">{{ message.time }}</strong>
           </div>
-          <div class="col-10 mb-1 small">{{ message.text }}</div>
+          <div class="col-10 mb-1 small">{{ message.message }}</div>
+          </div>
+        </div>
+        
         </div>
       </div>
     </div>
@@ -35,11 +40,13 @@
 
 <script>
 import Pusher from 'pusher-js';
-
+const pusher = new Pusher('468f0c2d729723ab5378', {
+  cluster: 'ap1'
+})
 export default {
   data() {
     return {
-      id_room: '',
+      id_room: '1',
       username: 'username',
       message: '',
       messages: [],
@@ -55,7 +62,7 @@ export default {
     // },
 
      async asyncData({$axios}) {
-      const listmessage =  await $axios.get('http://localhost:8000/api/listchat');
+      const listmessage =  await $axios.get('http://localhost:8000/api/list/room/chat');
       
       return {listmessage}
     },
@@ -64,38 +71,58 @@ export default {
   mounted() {
     Pusher.logToConsole = true;
 
-    const pusher = new Pusher('468f0c2d729723ab5378', {
-      cluster: 'ap1'
-    });
+    this.subscibeChannel()
+    // const pusher = new Pusher('468f0c2d729723ab5378', {
+    //   cluster: 'ap1'
+    // });
 
-    const channel = pusher.subscribe('chat');
-    channel.bind('message', data => {
-      this.messages.push(data);
-    });
+    // var channel = pusher.subscribe(this.id_room);
+    // channel.bind('message', data => {
+    //   this.messages.push(data);
+    // });
 
     this.chatlist.push(this.listmessage)
-
+    
   },
   methods: {
+    
+    subscibeChannel() {
+      const channel = pusher.subscribe(this.id_room);
+      return channel
+    },
+
+    unsubscribeChannel() {
+      pusher.unsubscribe(this.id_room);
+    },
+
+    bindingChannel() {
+      this.subscibeChannel.bind('message', data => {
+        this.message.push(data);
+      })
+    },
+
     async submit() {
-    await this.$axios.post('http://127.0.0.1:8000/api/messages', {
-        data: {
-           username: this.username,
-           message: this.message,
-           id_room : this.id_room,
-        }
-      });
       
+    await this.$axios.post('http://127.0.0.1:8000/api/messages', {
+           room_id : this.id_room,
+          message: this.message,
+      });
+       this.chatlist = []
+      const listmessage =   await this.$axios.get('http://localhost:8000/api/list/room/chat');
+      this.bindingChannel()
+      this.chatlist.push(listmessage)
       this.message = '';
     },
 
     async getMessage(id) {
-        this.id_room = id
-         this.messages = []
-      const data = await this.$axios.get(`http://127.0.0.1:8000/api/getmessage/${id}`);
+         this.id_room = id.toString()
+         this.unsubscribeChannel()
+         this.subscibeChannel()
+      const data = await this.$axios.get(`http://127.0.0.1:8000/api/chat/messages/${id}`);
       if(data.data.data.length===0){
          this.messages = []
       }else{
+         this.messages = []
         data.data.data.forEach(element => {
             this.messages.push(element)
           });
